b5f52ad08889be65c5afc4e71e91562f5d6f711a
Tushar Mathur <tusharmath@gmail.com>
2018-06-27 20:50:07 +0530

    fix(index): export concatC

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@
 
 export {ReducerFunction} from './src/ReducerFunction'
 export {CommandFunction} from './src/CommandFunction'
+export {concatC} from './src/concatC'
 export {concatR} from './src/concatR'
 export {matchC} from './src/matchC'
 export {matchR} from './src/matchR'
