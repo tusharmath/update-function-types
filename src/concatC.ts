268bcc266095ccd8275a698a5dc3eb77343c02b5
Tushar Mathur <tusharmath@gmail.com>
2018-07-01 02:05:21 +0530

    feat(concatC): make it associative
    concatC(concatC(a, b), c) === concatC(a, concatC(b, c))
    
    BREAKING CHANGE: concatC() is associative

--- src/concatC.ts
+++ src/concatC.ts
@@ -2,15 +2,19 @@
  * Created by tushar on 26/06/18
  */
 
-import {List, Action} from 'action-type'
+import {List, Action, isList} from 'action-type'
 import {CommandFunction} from './CommandFunction'
 import {CurriedFunction2, curry2} from 'ts-curry'
 
 export const concatC = <Input, State, Output>(
   ...t: Array<CommandFunction<Input, State, Output>>
-): CurriedFunction2<Input, State, Action<Array<Action<Output>>>> =>
+): CurriedFunction2<Input, State, Action<any>> =>
   curry2((input: Input, state: State) => {
     const result: Array<Action<Output>> = []
-    for (let i = 0; i < t.length; i++) result.push(t[i](input, state))
+    for (let i = 0; i < t.length; i++) {
+      const item = t[i](input, state)
+      if (isList(item)) result.push(...item.value)
+      else result.push(item)
+    }
     return List(...result)
   })
--- test/concatC.test.ts
+++ test/concatC.test.ts
@@ -8,21 +8,26 @@ import {action, List} from 'action-type'
 import {curry2} from 'ts-curry'
 
 describe('concatC', () => {
+  const P = (a: number, b: number) => action('+', a + b)
+  const M = (a: number, b: number) => action('*', a * b)
+  const D = (a: number, b: number) => action('/', a / b)
   it('should combine multiple command functions into one', () => {
-    const actual = concatC(
-      curry2((a: number, b: number) => action('+', a + b)),
-      curry2((a: number, b: number) => action('*', a * b))
-    )(1, 2)
-
+    const actual = concatC(curry2(P), curry2(M))(1, 2)
     const expected = List(action('+', 3), action('*', 2))
+
     assert.deepEqual(actual, expected)
   })
+
   it('should be curried', () => {
-    const actual = concatC(
-      (a: number, b: number) => action('+', a + b),
-      (a: number, b: number) => action('*', a * b)
-    )(1)(2)
+    const actual = concatC(P, M)(1)(2)
     const expected = List(action('+', 3), action('*', 2))
+
     assert.deepEqual(actual, expected)
+  })
+
+  it('should be associative', () => {
+    const a = concatC(concatC(P, M), D)
+    const b = concatC(P, concatC(M, D))
+    assert.deepEqual(a(1, 2), b(1, 2))
   })
 })
