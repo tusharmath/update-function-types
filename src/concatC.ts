093fc593bdf50459fd29a087ebf68e00d4900d6e
Tushar Mathur <tusharmath@gmail.com>
2018-07-01 09:25:37 +0530

    feat(concatC): concatC is easier to compose
    concatC is associative and adhers to additive identity
    
    BREAKING CHANGE: The action tree is internally flattened

--- src/concatC.ts
+++ src/concatC.ts
@@ -2,10 +2,13 @@
  * Created by tushar on 26/06/18
  */
 
-import {List, Action, isList} from 'action-type'
+import {List, Action, isList, Nil} from 'action-type'
 import {CommandFunction} from './CommandFunction'
 import {CurriedFunction2, curry2} from 'ts-curry'
 
+const safeInsert = <T>(result: Array<Action<T>>, item: Action<T>) => {
+  if (item !== Nil) result.push(item)
+}
 export const concatC = <Input, State, Output>(
   ...t: Array<CommandFunction<Input, State, Output>>
 ): CurriedFunction2<Input, State, Action<any>> =>
@@ -13,8 +16,18 @@ export const concatC = <Input, State, Output>(
     const result: Array<Action<Output>> = []
     for (let i = 0; i < t.length; i++) {
       const item = t[i](input, state)
-      if (isList(item)) result.push(...item.value)
-      else result.push(item)
+      if (isList(item)) {
+        for (let i = 0; i < item.value.length; i++) {
+          safeInsert(result, item.value[i])
+        }
+      } else {
+        safeInsert(result, item)
+      }
     }
-    return List(...result)
+
+    return result.length === 0
+      ? Nil
+      : result.length === 1
+        ? result[0]
+        : List(...result)
   })
--- test/concatC.test.ts
+++ test/concatC.test.ts
@@ -4,13 +4,14 @@
 
 import * as assert from 'assert'
 import {concatC} from '../src/concatC'
-import {action, List} from 'action-type'
+import {action, List, Nil} from 'action-type'
 import {curry2} from 'ts-curry'
 
 describe('concatC', () => {
   const P = (a: number, b: number) => action('+', a + b)
   const M = (a: number, b: number) => action('*', a * b)
   const D = (a: number, b: number) => action('/', a / b)
+  const Z = () => Nil
   it('should combine multiple command functions into one', () => {
     const actual = concatC(curry2(P), curry2(M))(1, 2)
     const expected = List(action('+', 3), action('*', 2))
@@ -25,9 +26,21 @@ describe('concatC', () => {
     assert.deepEqual(actual, expected)
   })
 
-  it('should be associative', () => {
+  it('should be associative (P + M) + D === P + (M + D)', () => {
     const a = concatC(concatC(P, M), D)
     const b = concatC(P, concatC(M, D))
     assert.deepEqual(a(1, 2), b(1, 2))
+  })
+
+  it('should satisfy additive identity P + Z === P', () => {
+    const a = concatC(P, Z)
+    const b = P
+    assert.deepEqual(a(1, 2), b(1, 2))
+  })
+
+  it('should satisfy additive identity Z + Z === Z', () => {
+    const actual = concatC(Z, Z)(1, 2)
+    const expected = Nil
+    assert.deepEqual(actual, expected)
   })
 })
