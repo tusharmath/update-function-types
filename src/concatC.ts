67254dcd7b5debe814880568ac5f78c12c9e9f55
Tushar Mathur <tusharmath@gmail.com>
2018-07-01 13:02:06 +0530

    fix(package): update action-type dependency

--- src/concatC.ts
+++ src/concatC.ts
@@ -2,13 +2,10 @@
  * Created by tushar on 26/06/18
  */
 
-import {List, Action, isList, Nil} from 'action-type'
+import {Action, isList, isNil, List, Nil} from 'action-type'
 import {CommandFunction} from './CommandFunction'
 import {CurriedFunction2, curry2} from 'ts-curry'
 
-const safeInsert = <T>(result: Array<Action<T>>, item: Action<T>) => {
-  if (item !== Nil) result.push(item)
-}
 export const concatC = <Input, State, Output>(
   ...t: Array<CommandFunction<Input, State, Output>>
 ): CurriedFunction2<Input, State, Action<any>> =>
@@ -16,13 +13,11 @@ export const concatC = <Input, State, Output>(
     const result: Array<Action<Output>> = []
     for (let i = 0; i < t.length; i++) {
       const item = t[i](input, state)
-      if (isList(item)) {
+      if (isList(item))
         for (let i = 0; i < item.value.length; i++) {
-          safeInsert(result, item.value[i])
+          if (!isNil(item.value[i])) result.push(item.value[i])
         }
-      } else {
-        safeInsert(result, item)
-      }
+      else if (!isNil(item)) result.push(item)
     }
 
     return result.length === 0
